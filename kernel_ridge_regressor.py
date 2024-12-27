1be1a838f1fe4a298cec07e7ec8d4bb1c124d33b
tejas nayak <136724042+tejasnayak1@users.noreply.github.com>
2024-12-27 10:26:55 +0530

    Add files via upload

---
+++ kernel_ridge_regressor.py
@@ -0,0 +1,51 @@
+
+import pandas
+
+dataset = pandas.read_csv("./dataset.csv")
+
+#|%%--%%| <vPPNELChVL|xz1vRACtPF>
+
+import sklearn.model_selection
+
+train: pandas.DataFrame
+test: pandas.pandas.DataFrame
+train, test = sklearn.model_selection.train_test_split(dataset, train_size=.8)
+y_train: pandas.DataFrame = train[["BTE_percentage", "BSFC_kg_per_kWh", "NOx_ppm", "HC_ppm", "CO_percentage", "CO2_percentage"]]
+x_train = train[train.columns.difference(["BTE_percentage", "BSFC_kg_per_kWh", "NOx_ppm", "HC_ppm", "CO_percentage", "CO2_percentage"])]
+y_test : pandas.DataFrame= test[["BTE_percentage", "BSFC_kg_per_kWh", "NOx_ppm", "HC_ppm", "CO_percentage", "CO2_percentage"]]
+x_test : pandas.DataFrame= test[test.columns.difference(["BTE_percentage", "BSFC_kg_per_kWh", "NOx_ppm", "HC_ppm", "CO_percentage", "CO2_percentage"])]
+
+#|%%--%%| <xz1vRACtPF|lZOLt6stwb>
+
+
+import sklearn.kernel_ridge
+import matplotlib.pyplot as plt
+import sklearn.metrics
+
+
+for output_parameter in ["BTE_percentage", "BSFC_kg_per_kWh", "NOx_ppm", "HC_ppm", "CO_percentage", "CO2_percentage"]:
+    current_y_train = y_train[output_parameter].to_numpy()
+    scale = sum(current_y_train) / len(current_y_train)
+    min_y = min(current_y_train)
+    max_y = max(current_y_train)
+    current_y_train = current_y_train / scale
+    model = sklearn.kernel_ridge.KernelRidge()
+    model = model.fit(x_train, current_y_train)
+    train_pred = model.predict(x_train) * scale
+    test_pred = model.predict(x_test) * scale
+    plt.scatter(y_test[output_parameter],  test_pred, marker="x", color="red", label="Testing set")
+    plt.scatter(y_train[output_parameter], train_pred, marker="*", color="blue", label="Training set")
+    plt.legend()
+    plt.text(min_y, max_y, f"MSE Train : {sklearn.metrics.mean_squared_error(train_pred, y_train[output_parameter]):.3f}, MSE Test  : {sklearn.metrics.mean_squared_error(test_pred, y_test[output_parameter]):.3f}")
+    plt.text(min_y, max_y * .95, f"MAE Train  : {sklearn.metrics.mean_absolute_error(train_pred, y_train[output_parameter]):.3f} MAE Test  : {sklearn.metrics.mean_absolute_error(test_pred, y_test[output_parameter]):.3f}")
+    plt.text(min_y, max_y * .9, f"R2 Train  : {sklearn.metrics.r2_score(train_pred, y_train[output_parameter]):.3f} R2 Test  : {sklearn.metrics.r2_score(test_pred, y_test[output_parameter]):.3f}")
+    plt.plot(
+            [min(y_train[output_parameter]), max(y_train[output_parameter])],
+            [min(y_train[output_parameter]), max(y_train[output_parameter])],
+            linestyle="dashed",
+            color="black"
+    )
+    plt.ylabel(f"Predicted {output_parameter}")
+    plt.xlabel(f"Actual {output_parameter}")
+    plt.title("Kernel Ridge Regression: Actual vs Predicted")
+    plt.show()
---
+++ random_forrest_regression.py
@@ -0,0 +1,44 @@
+import pandas
+
+dataset = pandas.read_csv("./dataset.csv")
+
+import sklearn.model_selection
+
+train: pandas.DataFrame
+test: pandas.DataFrame
+train, test = sklearn.model_selection.train_test_split(dataset, train_size=0.8, random_state=42)  # Set random_state
+y_train: pandas.DataFrame = train[["BTE_percentage", "BSFC_kg_per_kWh", "NOx_ppm", "HC_ppm", "CO_percentage", "CO2_percentage"]]
+x_train = train[train.columns.difference(["BTE_percentage", "BSFC_kg_per_kWh", "NOx_ppm", "HC_ppm", "CO_percentage", "CO2_percentage"])]
+y_test: pandas.DataFrame = test[["BTE_percentage", "BSFC_kg_per_kWh", "NOx_ppm", "HC_ppm", "CO_percentage", "CO2_percentage"]]
+x_test: pandas.DataFrame = test[test.columns.difference(["BTE_percentage", "BSFC_kg_per_kWh", "NOx_ppm", "HC_ppm", "CO_percentage", "CO2_percentage"])]
+
+import sklearn.ensemble
+import matplotlib.pyplot as plt
+import sklearn.metrics
+
+for output_parameter in ["BTE_percentage", "BSFC_kg_per_kWh", "NOx_ppm", "HC_ppm", "CO_percentage", "CO2_percentage"]:
+    current_y_train = y_train[output_parameter].to_numpy()
+    scale = sum(current_y_train) / len(current_y_train)
+    min_y = min(current_y_train)
+    max_y = max(current_y_train)
+    current_y_train = current_y_train / scale
+    model = sklearn.ensemble.RandomForestRegressor()  # Set random_state
+    model = model.fit(x_train, current_y_train)
+    train_pred = model.predict(x_train) * scale
+    test_pred = model.predict(x_test) * scale
+    plt.scatter(y_test[output_parameter], test_pred, marker="x", color="red", label="Testing set")
+    plt.scatter(y_train[output_parameter], train_pred, marker="*", color="blue", label="Training set")
+    plt.legend()
+    plt.text(min_y, max_y, f"MSE Train: {sklearn.metrics.mean_squared_error(train_pred, y_train[output_parameter]):.3f}, MSE Test: {sklearn.metrics.mean_squared_error(test_pred, y_test[output_parameter]):.3f}")
+    plt.text(min_y, max_y * 0.95, f"MAE Train: {sklearn.metrics.mean_absolute_error(train_pred, y_train[output_parameter]):.3f} MAE Test: {sklearn.metrics.mean_absolute_error(test_pred, y_test[output_parameter]):.3f}")
+    plt.text(min_y, max_y * 0.9, f"R2 Train: {sklearn.metrics.r2_score(train_pred, y_train[output_parameter]):.3f} R2 Test: {sklearn.metrics.r2_score(test_pred, y_test[output_parameter]):.3f}")
+    plt.plot(
+        [min(y_train[output_parameter]), max(y_train[output_parameter])],
+        [min(y_train[output_parameter]), max(y_train[output_parameter])],
+        linestyle="dashed",
+        color="black"
+    )
+    plt.ylabel(f"Predicted {output_parameter}")
+    plt.xlabel(f"Actual {output_parameter}")
+    plt.title("Random Forest Regression: Actual vs Predicted")
+    plt.show()
